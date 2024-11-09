7c54a5277bc02adad572911cbf115c896a89d8f1
Jaimito <jaijo64@gmail.com>
2024-11-09 02:04:22 -0400

    TextoNavbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,24 +1,24 @@
 <template>
     <nav class="bg-primary text-light py-4 shadow-lg">
       <div class="container mx-auto flex justify-between items-center">
-        <router-link to="/" class="text-2xl font-semibold">
+        <router-link to="/" class="text-2xl font-semibold text-dark">
           Comida Rápida
         </router-link>
         <ul class="flex space-x-6">
-          <li>
-            <router-link to="/" class="hover:text-secondary">Inicio</router-link>
+          <li class="list-none">
+            <router-link to="/" class="text-dark hover:text-secondary">Inicio</router-link>
           </li>
-          <li>
-            <router-link to="/menu" class="hover:text-secondary">Menú</router-link>
+          <li class="list-none">
+            <router-link to="/menu" class="text-dark hover:text-secondary">Menú</router-link>
           </li>
-          <li>
-            <router-link to="/promociones" class="hover:text-secondary">Promociones</router-link>
+          <li class="list-none">
+            <router-link to="/promociones" class="text-dark hover:text-secondary">Promociones</router-link>
           </li>
-          <li>
-            <router-link to="/about" class="hover:text-secondary">Acerca de</router-link>
+          <li class="list-none">
+            <router-link to="/about" class="text-dark hover:text-secondary">Acerca de</router-link>
           </li>
-          <li>
-            <router-link to="/contact" class="hover:text-secondary">Contacto</router-link>
+          <li class="list-none">
+            <router-link to="/contact" class="text-dark hover:text-secondary">Contacto</router-link>
           </li>
         </ul>
       </div>
@@ -26,6 +26,7 @@
   </template>
   
   <style scoped>
+  /* Elimina los puntos de lista */
   li {
     list-style: none;
   }
